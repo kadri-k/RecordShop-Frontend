14dae2cb11653b067c0965384e0df48543d90b42
kadri-k <kklampe@gmail.com>
2024-12-16 14:54:41 +0000

    feat: add Retrofit.Builder method

--- app/src/main/java/com/northcoders/recordshop/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/recordshop/service/RetrofitInstance.java
@@ -1,9 +1,19 @@
 package com.northcoders.recordshop.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 
 public class RetrofitInstance {
+
     private static Retrofit retrofit = null;
 
     private final static String BASE_URL = "http://10.0.2.2:8080/api/albums/";
+
+     private static AlbumApiService getService() {
+         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+     }
 }
