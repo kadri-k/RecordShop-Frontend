00d4117fb59db8a9af4cf9bdb0150f032218698c
kadri-k <kklampe@gmail.com>
2024-12-16 13:22:56 +0000

    feat: add key values, contructor and getters and setters to Album class

--- app/src/main/java/com/northcoders/recordshop/model/Album.java
+++ app/src/main/java/com/northcoders/recordshop/model/Album.java
@@ -1,4 +1,72 @@
 package com.northcoders.recordshop.model;
 
 public class Album {
+    private long id;
+    private String title;
+    private String artist;
+    private String genre;
+    private int stock;
+    double price;
+
+    public Album(long id, double price, String title, String artist, String genre, int stock) {
+        this.id = id;
+        this.price = price;
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.stock = stock;
+    }
+    public Album() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
+
+
+
