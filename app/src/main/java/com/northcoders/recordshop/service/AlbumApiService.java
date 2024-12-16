1f6037af593325de8a37cac0f608572b7bf92baa
kadri-k <kklampe@gmail.com>
2024-12-16 15:46:43 +0000

    feat: implement method to return call object of the list of album objects

--- app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshop/service/AlbumApiService.java
@@ -1,4 +1,14 @@
 package com.northcoders.recordshop.service;
 
+import com.northcoders.recordshop.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AlbumApiService {
+
+    @GET("Album")
+    Call<List<Album>> getAllAlbums();
 }
