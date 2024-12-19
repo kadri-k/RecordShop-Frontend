beb4a8147348a57429f0ae274c7984d7aa924e3c
kadri-k <kklampe@gmail.com>
2024-12-19 11:05:27 +0000

    feat: create logic in OnCreate method of the AddNewAlbumActivity

--- app/src/main/java/com/northcoders/recordshop/ui/mainactivity/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/addalbum/AddNewAlbumActivity.java
@@ -7,20 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.northcoders.recordshop.R;
 import com.northcoders.recordshop.databinding.ActivityAddNewAlbumBinding;
+import com.northcoders.recordshop.model.Album;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
 
     private ActivityAddNewAlbumBinding binding;
-    private
+    private AddNewAlbumClickHandler handler;
+    private Album album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_new_album);
+
+        album = new Album();
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_add_new_album
+        );
 
     }
 }
