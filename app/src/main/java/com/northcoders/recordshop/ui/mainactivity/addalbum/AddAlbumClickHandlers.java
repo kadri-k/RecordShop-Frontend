1942ceb396a784b462e999f7abe6f7c3b88322aa
kadri-k <kklampe@gmail.com>
2024-12-19 01:41:20 +0000

    feat: implement click handlers

---
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/addalbum/AddAlbumClickHandlers.java
@@ -0,0 +1,42 @@
+package com.northcoders.recordshop.ui.mainactivity.addalbum;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.ui.mainactivity.MainActivity;
+import com.northcoders.recordshop.ui.mainactivity.MainActivityViewModel;
+
+
+public class AddAlbumClickHandlers {
+
+    private Album album;
+    private Context context;
+    private MainActivityViewModel viewModel;
+
+    public AddAlbumClickHandlers(Album album, MainActivityViewModel viewModel, Context context) {
+        this.album = album;
+        this.viewModel = viewModel;
+        this.context = context;
+    }
+    public void onSubmitBtnClicked(View view) {
+        //conditional check whether fields are empty
+        if(album.getTitle() == null || album.getArtist() == null || album.getGenre() == null || album.getPrice() == null) {
+            Toast.makeText(context, "Fields cannot be empty!", Toast.LENGTH_SHORT).show();
+        } else {
+
+            Intent intent = new Intent(context, MainActivity.class);
+
+            viewModel.addAlbum(album);
+
+            context.startActivity(intent);
+        }
+    }
+    public void onBackBtnClicked(View view) {
+        Intent intent = new Intent (context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+}
